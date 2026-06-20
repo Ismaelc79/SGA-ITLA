2008b1d3917ab1ecd2e5b145d7be6ff5959265a1
Salim Ismael Basora De la Cruz <Star_rock751@hotmail.com>
2026-06-20 00:33:45 -0400

    Cambios realizados a IBaseRepository

--- SGA.Persistence/Base/IBaseRepository.cs
+++ SGA.Persistence/Base/IBaseRepository.cs
@@ -1,7 +1,9 @@
 ﻿namespace SGA.Persistence.Repository.Base
 {
-    public interface IBaseRepository<T>
+    public interface IBaseRepository<T> where T : class
     {
+      //  protected readonly SGAContext _context;
+       // protected readonly DbSet<T> _dbSet;
         Task<T?> GetByIdAsync(int id);
 
         Task<IEnumerable<T>> GetAllAsync();
