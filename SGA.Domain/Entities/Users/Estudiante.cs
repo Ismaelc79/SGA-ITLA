2d598b9c5ed20732504c60d0c43147afe204c2d2
Salim Ismael Basora De la Cruz <Star_rock751@hotmail.com>
2026-06-19 23:13:56 -0400

    Cambios realizados en dominio

--- SGA.Domain/Entities/Users/Estudiante.cs
+++ SGA.Domain/Entities/Users/Estudiante.cs
@@ -7,6 +7,7 @@ namespace SGA.Domain.Entities.Users
         public int EstudianteId { get; set; }
         public int UsuarioId { get; set; }
         public string Nombre { get; set; }
+        public string Matricula { get; set; }
         public string Carrera { get; set; }
         public string PeriodoAcademico { get; set; }
         public string Estado { get; set; }
