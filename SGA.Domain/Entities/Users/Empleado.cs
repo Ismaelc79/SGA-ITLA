452ad3cab133e5e0402c7ac9797f3b2d11b0c409
Salim Ismael Basora De la Cruz <Star_rock751@hotmail.com>
2026-06-19 23:06:19 -0400

    Cambios realizados a entidades

--- SGA.Domain/Entities/Users/Empleado.cs
+++ SGA.Domain/Entities/Users/Empleado.cs
@@ -5,6 +5,7 @@ namespace SGA.Domain.Entities.Users
     public class Empleado : AuditEntity
     {
         public int EmpleadoId { get; set; }
+        public int UsuarioId { get; set; }
         public string Nombre { get; set; }
         public string Identificacion { get; set; }
         public string Cargo { get; set; }
--- SGA.Domain/Entities/Users/Estudiante.cs
+++ SGA.Domain/Entities/Users/Estudiante.cs
@@ -5,6 +5,7 @@ namespace SGA.Domain.Entities.Users
     public class Estudiante : AuditEntity
     {
         public int EstudianteId { get; set; }
+        public int UsuarioId { get; set; }
         public string Nombre { get; set; }
         public string Carrera { get; set; }
         public string PeriodoAcademico { get; set; }
