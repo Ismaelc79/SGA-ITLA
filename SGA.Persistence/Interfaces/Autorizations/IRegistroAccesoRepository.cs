0aae5dea0c58d3b348b188988c071c84eac5a0c2
Salim Ismael Basora De la Cruz <Star_rock751@hotmail.com>
2026-06-30 18:50:27 -0400

    Cambios realizados a la capa persistencia

--- SGA.Persistence/Interfaces/Autorizations/IRegistroAccesoRepository.cs
+++ SGA.Persistence/Interfaces/Autorizations/IRegistroAccesoRepository.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace SGA.Persistence.Interfaces.Autorizations
 {
-    public interface IRegistroAccesoRepositorycs
+    public interface IRegistroAccesoRepository
     {
     }
 }
