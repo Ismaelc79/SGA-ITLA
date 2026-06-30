c9ba4d5389809dd1d47aa56317eca8c71b7efdc2
Salim Ismael Basora De la Cruz <Star_rock751@hotmail.com>
2026-06-30 18:01:42 -0400

    Cambios realizados orientados a persistencia

--- SGA.Api/Program.cs
+++ SGA.Api/Program.cs
@@ -1,8 +1,11 @@
+using SGA.Persistence.ServiceExtensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
