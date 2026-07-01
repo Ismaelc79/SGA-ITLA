374865934d64a7712fb87b8465d020b0690e1dc0
Salim Ismael Basora De la Cruz <Star_rock751@hotmail.com>
2026-06-30 21:55:45 -0400

    Cambios orientados a persistencia

--- SGA.Persistence/ServiceExtensions/DependencyInjection.cs
+++ SGA.Persistence/ServiceExtensions/DependencyInjection.cs
@@ -2,6 +2,13 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SGA.Persistence.Context;
+using SGA.Persistence.Interfaces;
+using SGA.Persistence.Interfaces.Autorizations;
+using SGA.Persistence.Interfaces.Trips;
+using SGA.Persistence.Interfaces.Users;
+using SGA.Persistence.Repositories.Autorizations;
+using SGA.Persistence.Repositories.Trips;
+using SGA.Persistence.Repositories.Users;
 
 namespace SGA.Persistence.ServiceExtensions
 {
@@ -15,9 +22,28 @@ namespace SGA.Persistence.ServiceExtensions
             var connectionString = configuration.GetConnectionString("DefaultConnection");
 
             services.AddDbContext<SGADB>(options =>
-                    options.UseSqlServer(connectionString));               
-                
-                return services;
+                    options.UseSqlServer(connectionString));    
+            
+            services.AddScoped<IAutorizacionRepository, AutorizacionRepository>();
+            services.AddScoped<IPagoRepository, PagoRepository>();
+            services.AddScoped<IRegistroAccesoRepository, RegistroAccesoRepository>();
+            services.AddScoped<ITarjetaRecargableRepository, TarjetaRecargableRepository>();
+            services.AddScoped<ITicketRepository, TicketRepository>();
+
+            services.AddScoped<IBusRepository, BusRepository>();
+            services.AddScoped<IHorarioRepository, HorarioRepository>();
+            services.AddScoped<IIncidenciaRepository, IncidenciaRepository>();
+            services.AddScoped<INotificacionRepository, NotificacionRepository>();
+            services.AddScoped<IParadaRepository, ParadaRepository>();
+            services.AddScoped<IRutaRepository, RutaRepository>();
+            services.AddScoped<IViajeRepository, ViajeRepository>();
+
+            services.AddScoped<IConductorRepository, ConductorRepository>();
+            services.AddScoped<IEmpleadoRepository, EmpleadoRepository>();
+            services.AddScoped<IEstudianteRepository, EstudianteRepository>();
+            services.AddScoped<IRolRepository, RolRepository>();
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            return services;
         }
        
 
