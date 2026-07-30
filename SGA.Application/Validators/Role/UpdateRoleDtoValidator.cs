a68392cff1bdd1c335f3541f723db2a49b1092aa
Salim Ismael <Ismaell_751@hotmail.com>
2026-07-30 09:54:44 -0400

    Cambios orientados a la reconstruccion del proyecto

---
+++ SGA.Application/Validators/Role/UpdateRoleDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using SGA.Application.DTOs.Role;
+using SGA.Application.Validators.Common;
+
+namespace SGA.Application.Validators.Role
+{
+    public sealed class UpdateRoleDtoValidator : AbstractValidator<UpdateRoleDto>
+    {
+        public UpdateRoleDtoValidator()
+        {
+            RuleFor(x => x.Nombre)
+                .NotEmpty()
+                    .WithMessage(ValidationMessages.Requerido("El nombre del rol"))
+                .MaximumLength(ValidationConstants.LongitudTextoCorta)
+                    .WithMessage(ValidationMessages.LongitudMaxima("El nombre del rol", ValidationConstants.LongitudTextoCorta));
+
+            RuleFor(x => x.Descripcion)
+                .MaximumLength(ValidationConstants.LongitudTextoLarga)
+                    .WithMessage(ValidationMessages.LongitudMaxima("La descripción", ValidationConstants.LongitudTextoLarga));
+        }
+    }
+}
---
+++ SGA.Application/Validators/User/CreateUserDtoValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using SGA.Application.DTOs.User;
+using SGA.Application.Validators.Common;
+
+namespace SGA.Application.Validators.User
+{
+    public sealed class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
+    {
+        private const int LongitudMinimaUserName = 3;
+        private const int LongitudMaximaUserName = 100;
+        private const int LongitudMinimaPassword = 6;
+
+        public CreateUserDtoValidator()
+        {
+            RuleFor(x => x.UserName)
+                .NotEmpty()
+                    .WithMessage(ValidationMessages.Requerido("El nombre de usuario"))
+                .MinimumLength(LongitudMinimaUserName)
+                    .WithMessage(ValidationMessages.LongitudMinima("El nombre de usuario", LongitudMinimaUserName))
+                .MaximumLength(LongitudMaximaUserName)
+                    .WithMessage(ValidationMessages.LongitudMaxima("El nombre de usuario", LongitudMaximaUserName));
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                    .WithMessage(ValidationMessages.Requerido("El correo"))
+                .EmailAddress()
+                    .WithMessage("El correo no tiene un formato válido.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                    .WithMessage(ValidationMessages.Requerido("La contraseña"))
+                .MinimumLength(LongitudMinimaPassword)
+                    .WithMessage(ValidationMessages.LongitudMinima("La contraseña", LongitudMinimaPassword));
+        }
+    }
+}
